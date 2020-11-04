0e13e225ee4074f0d85d56925fba3cb684d27a16
Rayen RAFFA <raffa.rayen@gmail.com>
2020-11-04 15:07:23 +0100

    add script

---
+++ script.py
@@ -0,0 +1,27 @@
+# BEFORE YOU START
+
+# Javascript uses curly brackets {} to delimit code blocks
+# Python, however, uses indentation to delimit code blocks
+
+# Let's consider this Javascript code 
+
+# if(hungry) {
+# eat();
+# } else { dont_eat();
+# }
+
+
+# Notice that the above code does not care about indentation.
+# In Python, we NEED to write it this way 
+
+# if(hungry):
+#     eat()
+# else:
+#     dont_eat()
+
+# With that information in mind, happy hacking :)
+
+#########
+# INSERT YOUR CODE BELOW THIS LINE
+#########
+
